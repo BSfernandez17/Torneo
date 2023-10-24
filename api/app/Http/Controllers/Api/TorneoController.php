d7053888d828dbca3368cf196192d7caf5c55c5f
Brayan Fernandez <brayanfernandez1706@gmail.com>
2023-10-23 20:21:54 -0500

    funciona bien pero los byes siempre se le asignan al mismo

--- api/app/Http/Controllers/Api/TorneoController.php
+++ api/app/Http/Controllers/Api/TorneoController.php
@@ -42,7 +42,7 @@ class TorneoController extends Controller
         $numAjustes = $numParticipantes - (2 ** ($numJornadas - 1));
         $bracketPrincipal= [];
         $bracketPerdedores=[];
-        $bracketGanadores=[];
+        $ganadoresRonda=[];
         $enfrentamientosGanadores=[];
         $enfrentamientosPerdedores=[];
         if ($numParticipantes < 2) {
@@ -52,46 +52,76 @@ class TorneoController extends Controller
         for($i=0;$i<$numParticipantes;$i++){
             $bracketPrincipal [$i]=$this->participantes[$i];
         }
-        while (count($bracketPrincipal) > 1) {
+        while ($ronda <=$numJornadas) {
             $p1 = null;
             $p2 = null;
             $ganador = null;
-
+            $numParticipantes = count($bracketPrincipal);
             for ($i = 0; $i < $numParticipantes; $i += 2) {
                 if (isset($bracketPrincipal[$i]) && isset($bracketPrincipal[$i + 1])) {
                     $p1 = $bracketPrincipal[$i];
                     $p2 = $bracketPrincipal[$i + 1];
                     $ganador = $this->simularEnfrentamiento($p1, $p2);
-                    $bracketGanadores[]=$ganador;
+                    
                 } else {
                     if (isset($bracketPrincipal[$i])) {
                         $p1 = $bracketPrincipal[$i];
                         $p2 = 'Bye';
                         $ganador = $p1;
-                        $bracketGanadores[]=$ganador;
                     } elseif (isset($bracketPrincipal[$i + 1])) {
                         $p1 = 'Bye';
                         $p2 = $bracketPrincipal[$i + 1];
                         $ganador = $p2;
-                        $bracketGanadores[]=$ganador;
                     }
+                    
+                }
+                $ganadoresRonda[]=$ganador;
+                if ($p1 != $ganador && $p1 != 'Bye') {
+                    $bracketPerdedores[] = $p1;
+                } elseif ($p2 != $ganador && $p2 != 'Bye') {
+                    $bracketPerdedores[] = $p2;
                 }
                 $enfrentamientosGanadores[] = [
                     'Participante 1' => $p1,
                     'Participante 2' => $p2,
                     'GANADOR' => $ganador,
+                    'Ronda' => $ronda
                 ];
-                
+
             }
-            break;
+            array_splice($bracketPrincipal, 0);
+            for($j=0;$j<count($ganadoresRonda);$j++){
+                $bracketPrincipal [$j]=$ganadoresRonda[$j];
+            }
+            array_splice($ganadoresRonda, 0);
+            $ronda++;
         }
         
 
         return [
             'Enfrentamientos'=> $enfrentamientosGanadores,
+            'Ganadores' => $bracketPrincipal,
+            'Perdedores' =>$bracketPerdedores
         ];
     }
 }
+/*
+                if ($p1 != $ganador && $p1 != 'Bye') {
+                    $bracketPerdedores[] = $p1;
+                    $indexP1 = array_search($p1, $bracketPrincipal);
+                    unset($bracketPrincipal[$indexP1]);
+
+                } elseif ($p2 != $ganador && $p2 != 'Bye') {
+                    $bracketPerdedores[] = $p2;
+                    $indexP2 = array_search($p2, $bracketPrincipal);
+                    unset($bracketPrincipal[$indexP2]);
+                } 
+                
+                
+                            unset($bracketPrincipal);
+            for($j=0;$j<count($ganadoresRonda);$j++){
+                $bracketPrincipal [$j]=$ganadoresRonda[$j];
+            }*/
 
 
 
